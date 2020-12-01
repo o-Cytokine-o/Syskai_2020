02cf975609ebef5b9713c51ae5640c11fef55d1e
okaasandesu <b7070@oic.jp>
2020-12-01 14:31:34 +0900

    経路ちゃんとしたやつ

--- AR_Augment_New/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/Keiro.cs
+++ AR_Augment_New/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/Keiro.cs
@@ -16,15 +16,30 @@ public class Keiro : MonoBehaviour
 
         //Add(枝元,枝先,重み)
         //頂点n個分の情報を手動で追加
-        graph.Add(0, 1, 1);
+        // 辺の情報を追加する(無向グラフなので両方の向きに)
+        graph.Add(0, 1, 7);
+        graph.Add(0, 2, 14);
+        graph.Add(0, 3, 9);
+        graph.Add(1, 0, 7);
+        graph.Add(1, 3, 10);
+        graph.Add(1, 4, 15);
+        graph.Add(2, 0, 14);
+        graph.Add(2, 3, 2);
+        graph.Add(2, 5, 9);
+        graph.Add(3, 0, 9);
+        graph.Add(3, 2, 2);
+        graph.Add(3, 4, 11);
+        graph.Add(4, 1, 15);
+        graph.Add(4, 3, 11);
+        graph.Add(4, 5, 6);
+        graph.Add(5, 2, 9);
+        graph.Add(5, 4, 6);　
     }
 
-    private int GetMindistance(int nowVertex, int toVertex)
+    private Dijkstra.Result GetMindistance(int nowVertex, int toVertex)
     {
-        var mindistance = graph.GetMinCost(nowVertex);
-        int distance = mindistance[toVertex];
-
-        return distance;
+        Dikstra.Result result = graph.GetMinCost(nowVertex, toVertex);
+        return result;
     }
 
     private var GetKeiro(int nowVertex){
